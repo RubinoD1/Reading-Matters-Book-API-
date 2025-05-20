38507adff24672730dda98fddff28b2846135744
RubinoD1 <Rubinod1@gmail.com>
2025-05-19 21:33:26 -0400

    added search check to verify that a value has been entered in the search input

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,24 +6,33 @@ const searchInput = document.querySelector(".searchInput");
 bookSearch.addEventListener("submit", async event => {
     //prevents page refresh
     event.preventDefault();
-    
+
     //search is used as a query parameter =?${} value that is passed into the api call
     let search = searchInput.value; 
     console.log(search, " is search");
 
-    //get radio checked value 
-    const radioValueCheck = document.getElementsByName("search-radio");
-    let radioSelection = "";
-    // iterate through radio buttons and set radioSelection to the name of checked radio button
-    for(i = 0; i < radioValueCheck.length; i++) {
-        if (radioValueCheck[i].checked){
-            radioSelection = radioValueCheck[i].value;
-        }
-    }
-
-    console.log(radioSelection, " is radio selection");
+    if (search){//if there is a value this will be true 
+        //get radio checked value 
+        const radioValueCheck = document.getElementsByName("search-radio");
+        let radioSelection = "";
     
-    //switch statement to select api call -- pass search value and radio value in function call 
+        // iterate through radio buttons and set radioSelection to the name of checked radio button
+        for(i = 0; i < radioValueCheck.length; i++) {
+            if (radioValueCheck[i].checked){
+                radioSelection = radioValueCheck[i].value;
+            }
+        }
+
+        console.log(radioSelection, " is radio selection");
+    
+        //switch statement to select api call -- pass search value and radio value in function call 
+
+
+    } else { //If no value is entered in search 
+        
+        console.log("Please enter a search parameter");
+    
+    }
     
 
 }); 
