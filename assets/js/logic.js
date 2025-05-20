744c32449b79d08425a036e2e32a66fc402ee3ce
RubinoD1 <Rubinod1@gmail.com>
2025-05-19 21:27:42 -0400

    event listener added to JS file that gets search value and radio btn selection

---
+++ assets/js/logic.js
@@ -0,0 +1,30 @@
+//bookSearch is form 
+const bookSearch = document.querySelector(".bookSearch");
+// searchInput is text box 
+const searchInput = document.querySelector(".searchInput");
+
+bookSearch.addEventListener("submit", async event => {
+    //prevents page refresh
+    event.preventDefault();
+    
+    //search is used as a query parameter =?${} value that is passed into the api call
+    let search = searchInput.value; 
+    console.log(search, " is search");
+
+    //get radio checked value 
+    const radioValueCheck = document.getElementsByName("search-radio");
+    let radioSelection = "";
+    // iterate through radio buttons and set radioSelection to the name of checked radio button
+    for(i = 0; i < radioValueCheck.length; i++) {
+        if (radioValueCheck[i].checked){
+            radioSelection = radioValueCheck[i].value;
+        }
+    }
+
+    console.log(radioSelection, " is radio selection");
+    
+    //switch statement to select api call -- pass search value and radio value in function call 
+    
+
+}); 
+
